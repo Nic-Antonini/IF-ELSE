d1f6db3ebdc55772796f47e25517f46c489c72f0
nicolas antonini <121265107+Nic-Antonini@users.noreply.github.com>
2023-09-20 00:23:38 -0300

    Add files via upload

--- assets/js/ex23.js
+++ assets/js/ex23.js
@@ -1,23 +1,4 @@
-function calculaDiaria(form){
-    var total;
-    var name = document.getElementById("name");
-    var days = parseInt(document.getElementById("days").value)
-    var type = document.getElementById("type")
-
-    if(document.getElementById("type").value === "simples"){
-        total = days * 100
-    }
-    else if(document.getElementById("type").value === "Médio"){
-        total = days * 200
-    }
-    else{
-        total = days * 300
-    }
-
-    alert(
-        "REGISTRO DA DIÁRIA: \n"+
-        "Nome do hóspede: "+name.value+
-        "\nTipo de apartamento: "+type.value+
-        "\nValor total: R$"+total.toFixed(2)
-    )
+function mostraElemento(form){
+    alert("Elemento: " + (form.elementos.selectedIndex)
+     + "\nValor do Elemento: " + (form.elementos.value))
 }
